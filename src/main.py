3cea0fb43a57e33062b58d864876f7c3a583f56c
Franz <franze2019@gmail.com>
2021-08-17 21:54:26 -0700

    updated !stream command in main.py
    !stream command now checks if there is a role called 'Notify'. If the server has a 'Notify' role, the bot will ping/@ the role whenever a new reddit post is posted.

--- src/main.py
+++ src/main.py
@@ -5,17 +5,18 @@ from discord.embeds import Embed
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord import message
+from discord.utils import get
 from discord.ext import tasks
 
-#Loads the .env file.
+# Loads the .env file.
 load_dotenv()
 
 running = False
 
-#Command prefix used for commands on Discord
+# Command prefix used for commands on Discord
 client = commands.Bot(command_prefix = '!')
 
-#Creates a read-only Reddit instances. Read-only instances are able to retrieve information from subreddits
+# Creates a read-only Reddit instances. Read-only instances are able to retrieve information from subreddits
 reddit = asyncpraw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
@@ -36,26 +37,29 @@ reddit = asyncpraw.Reddit(
     timeout=16,
 )
 
-#Keywords for the command !streamkeywords
+# Keywords for the command !streamkeywords
 userKeywords = []
 
-#When the bot is running, it sends a message in the terminal saying that they have logged in.
+# Checks if the server has the role "Notify". 
+active_role = False
+
+# When the bot is running, it sends a message in the terminal saying that they have logged in.
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
-#Error handling
+# Error handling
 @client.event
 async def on_command_error(ctx, error):
-    #If a user inputs a command that bot does not recognize, it sends a message that the command was not found.
+    # If a user inputs a command that bot does not recognize, it sends a message that the command was not found.
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Command not found.")
     
-    #If a user inputs a command but leaves the argument empty, it sends a message that the user is missing a required argument.
+    # If a user inputs a command but leaves the argument empty, it sends a message that the user is missing a required argument.
     if isinstance (error, commands.MissingRequiredArgument):
         await ctx.send("Missing required argument")
 
-#Displays all the commands that are available to use 
+# Displays all the commands that are available to use 
 @client.command()
 async def allcommands(ctx):
     display_embed=discord.Embed(title="RedditPost Bot 🐢", description="All the commands that are available to use!", color=0x007514)
@@ -64,27 +68,27 @@ async def allcommands(ctx):
     display_embed.add_field(name="Filters: ", value="hour, day, month, year, all, hour", inline=True)
     await ctx.send(embed = display_embed)
 
-#Adds a word into userKeyword list
+# Adds a word into userKeyword list
 @client.command()
 async def add(ctx, keyword):
     userKeywords.append(keyword)
     await ctx.send("Added " + keyword + " as a keyword")
 
-#Displays the userKeyword list
+# Displays the userKeyword list
 @client.command()
 async def show(ctx):
     for keyword in range(len(userKeywords)):
         await ctx.send(userKeywords[keyword])
 
-#Deletes all the keywords in the userKeyword list
+# Deletes all the keywords in the userKeyword list
 @client.command()
 async def clear(ctx):
     userKeywords.clear()
     await ctx.send("Cleared keyword list")
 
-#Displays the top 50 posts of the user's requested subreddit during a specific time.
+# Displays the top 50 posts of the user's requested subreddit during a specific time.
 @client.command()
-async def showtop(ctx, input_subreddit, filter):
+async def top(ctx, input_subreddit, filter):
 
     filter_list = ["day", "week", "month", "year", "all", "hour"]
 
@@ -106,11 +110,11 @@ async def showtop(ctx, input_subreddit, filter):
 
         async for submission in subreddit.top(filter, limit = 50):
             display_embed = discord.Embed(title = submission.title[0:256])
-            display_embed.set_author(name = "RedditPost Bot 🐢")
+            display_embed.set_author(name = "RedditPost Bot 🤖")
             display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
             display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
             display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
-            display_embed.set_thumbnail(url = "https://cdn.discordapp.com/attachments/435613438560043008/862045274037813258/375ce83551aafaec5f2d5ffef338b2fa.png")
+            display_embed.set_thumbnail(url = "https://cdn.pixabay.com/photo/2015/12/16/17/41/bell-1096280_960_720.png")
             await ctx.send(embed = display_embed)
 
         running = False
@@ -121,26 +125,26 @@ async def showtop(ctx, input_subreddit, filter):
     else:
         await ctx.send("Another command is running!")
 
-#Displays the hot 50 posts of the user's requested subreddit.
+# Displays the hot 50 posts of the user's requested subreddit.
 @client.command()
-async def showhot(ctx, input_subreddit):
+async def hot(ctx, input_subreddit):
 
     global running
 
     if running == False:
         running = True
-
+        
         await ctx.send("Posting hot 50 posts from " + input_subreddit)
 
         subreddit = await reddit.subreddit(input_subreddit)
 
         async for submission in subreddit.hot(limit = 50):
             display_embed = discord.Embed(title = submission.title[0:256])
-            display_embed.set_author(name = "RedditPost Bot 🐢")
+            display_embed.set_author(name = "RedditPost Bot 🤖")
             display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
             display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
             display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
-            display_embed.set_thumbnail(url = "https://cdn.discordapp.com/attachments/435613438560043008/862045274037813258/375ce83551aafaec5f2d5ffef338b2fa.png")
+            display_embed.set_thumbnail(url = "https://cdn.pixabay.com/photo/2015/12/16/17/41/bell-1096280_960_720.png")
             await ctx.send(embed = display_embed)
 
         running = False
@@ -148,9 +152,9 @@ async def showhot(ctx, input_subreddit):
     else:
         await ctx.send("Another command is running!")
 
-#Displays the newest 50 posts of the user's requested subreddit
+# Displays the newest 50 posts of the user's requested subreddit
 @client.command()
-async def shownew(ctx, input_subreddit):
+async def new(ctx, input_subreddit):
 
     global running
 
@@ -163,11 +167,11 @@ async def shownew(ctx, input_subreddit):
 
         async for submission in subreddit.new(limit = 50):
             display_embed = discord.Embed(title = submission.title[0:256])
-            display_embed.set_author(name = "RedditPost Bot 🐢")
+            display_embed.set_author(name = "RedditPost Bot 🤖")
             display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
             display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
             display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
-            display_embed.set_thumbnail(url = "https://cdn.discordapp.com/attachments/435613438560043008/862045274037813258/375ce83551aafaec5f2d5ffef338b2fa.png")
+            display_embed.set_thumbnail(url = "https://cdn.pixabay.com/photo/2015/12/16/17/41/bell-1096280_960_720.png")
             await ctx.send(embed = display_embed)
 
         running = False
@@ -175,62 +179,79 @@ async def shownew(ctx, input_subreddit):
     else:
         await ctx.send("Another command is running!")
 
-#Displays the newest posts of a subreddit and updates in real time whenever a new post is created. Additionally, if keywords are added, the bot will
-#only send posts to the chat that have keywords in the reddit post title. Runs until the user tells it to stop by using the command !stop
+# Displays the newest posts of a subreddit and updates in real time whenever a new post is created. Once a new post is created, the bot will @
+# the users in the 'Notify' role. Additionally, if keywords are added, the bot will only send posts to the chat that have keywords in the reddit post title. 
+# Runs until the user tells it to stop by using the command !stop
 @client.command()
 async def stream(ctx, input_subreddit):
 
     global streamloop
     global running
+    global active_role
 
-    if len(userKeywords) == 0:
-        if running == False:
-            running = True
-            @tasks.loop(seconds=1)
-            async def streamloop(inputsub):
-                subreddit = await reddit.subreddit(input_subreddit)
+    for role in ctx.guild.roles:
+        if role.name == 'Notify':
+            active_role = True
 
-                async for submission in subreddit.stream.submissions():
-                    display_embed = discord.Embed(title = submission.title[0:256])
-                    display_embed.set_author(name = "RedditPost Bot 🐢")
-                    display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
-                    display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
-                    display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
-                    display_embed.set_thumbnail(url = "https://cdn.discordapp.com/attachments/435613438560043008/862045274037813258/375ce83551aafaec5f2d5ffef338b2fa.png")
-                    await ctx.send(embed = display_embed)
-        else:
-            await ctx.send("Another command is running!")
-    else:
-        if running == False:
-            running = True
-            @tasks.loop(seconds=1)
-            async def streamloop(inputsub):
+    if active_role == True:
+        if len(userKeywords) == 0:
+            if running == False:
+                running = True
+                @tasks.loop(seconds=1)
+                async def streamloop(inputsub):
                     subreddit = await reddit.subreddit(input_subreddit)
 
-                    async for submission in subreddit.stream.submissions():
-                        subtitle = submission.title
-                        if any(keyword in subtitle for keyword in userKeywords):
-                            display_embed = discord.Embed(title = submission.title[0:256])
-                            display_embed.set_author(name = "RedditPost Bot 🐢")
-                            display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
-                            display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
-                            display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
-                            display_embed.set_thumbnail(url = "https://cdn.discordapp.com/attachments/435613438560043008/862045274037813258/375ce83551aafaec5f2d5ffef338b2fa.png")
-                            await ctx.send(embed = display_embed)
-        else:
-            await ctx.send("Another command is running!")
+                    notify = get(ctx.guild.roles, name = "Notify")
 
-        
+                    async for submission in subreddit.stream.submissions():
+                        await ctx.send(f"{notify.mention}")
+                        display_embed = discord.Embed(title = submission.title[0:256])
+                        display_embed.set_author(name = "RedditPost Bot 🤖")
+                        display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
+                        display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
+                        display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
+                        display_embed.set_thumbnail(url = "https://cdn.pixabay.com/photo/2015/12/16/17/41/bell-1096280_960_720.png")
+                        await ctx.send(embed = display_embed)
+            else:
+                await ctx.send("Another command is running!")
+        else:
+            if running == False:
+                running = True
+                @tasks.loop(seconds=1)
+                async def streamloop(inputsub):
+                        subreddit = await reddit.subreddit(input_subreddit)
+
+                        notify = get(ctx.guild.roles, name = "Notify")
+
+                        async for submission in subreddit.stream.submissions():
+                            subtitle = submission.title
+                            if any(keyword in subtitle for keyword in userKeywords):
+                                await ctx.send(f"{notify.mention}")
+                                display_embed = discord.Embed(title = submission.title[0:256])
+                                display_embed.set_author(name = "RedditPost Bot 🤖")
+                                display_embed.add_field(name =  "Subreddit:", value = input_subreddit, inline=False)
+                                display_embed.add_field(name =  "Link:", value = submission.shortlink, inline=True)
+                                display_embed.add_field(name =  "Posted by:", value = submission.author, inline=True)
+                                display_embed.set_thumbnail(url = "https://cdn.pixabay.com/photo/2015/12/16/17/41/bell-1096280_960_720.png")
+                                await ctx.send(embed = display_embed)
+            else:
+                await ctx.send("Another command is running!")
+    else: 
+        await ctx.send("Need to create the role '**Notify**' in order to get pinged whenever a new post is created.")
+        return None
+   
     streamloop.start(input_subreddit)
+    active_role = False
     await ctx.send("Streaming posts from " + input_subreddit)
 
-#Stops the !stream command.
+# Stops the !stream command.
 @client.command()
 async def stop(ctx):
     global running
     streamloop.cancel()
     running = False
-    await ctx.send("Successfully deactivated !stream")
+    active_role = False
+    await ctx.send("Successfully deactivated the stream")
 
 #Runs the bot
 client.run(os.getenv("DISCORD_TOKEN"))
